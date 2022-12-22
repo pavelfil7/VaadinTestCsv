bbb5193ce366c9b62d6d377132033c74c26b5f66
Pavel <Pavel7filatov@gmail.com>
2022-12-22 20:00:43 +0500

    Initial commit

---
+++ src/main/java/com/filatov/vaadintestcsv/VaadinTestCsvApplication.java
@@ -0,0 +1,13 @@
+package com.filatov.vaadintestcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinTestCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinTestCsvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filatov/vaadintestcsv/VaadinTestCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filatov.vaadintestcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinTestCsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
